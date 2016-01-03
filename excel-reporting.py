6c06daa63409a373c41b20b0ff81269e0fc00f4e
Joseph <wma_10@hotmail.com>
2016-01-03 18:02:59 +1100

    adding in excel reporting functions

--- excel-reporting.py
+++ excel-reporting.py
@@ -13,9 +13,9 @@ def main():
     ws2 = wb.get_sheet_by_name("Sheet 2")
     #ws2 = Import_Data(ws2, date)
     ws1 = Calculate_Totals(ws1, ws2)
-    Update_Month()
-    Update_Report_Range(date)
-    Update_Reported_Areas()
+    ws1 = Update_Month(date, ws1)
+    ws1 = Update_Report_Range(date, ws1)
+    ws1 = Update_Reported_Areas(ws1, ws2)
     wb.save('report.xlsm', as_template=False)
 
 #Step 1 Import in the rows from the db based on date range
@@ -28,21 +28,41 @@ def Get_Date():
 #Step 2 Recalculate totals
 def Calculate_Totals(ws1, ws2):
     #There is a title row we dont want to include so set row to 2
-    row = 2
-     
+    rows = 2
+    confirmed = 0
+    unconfirmed = 0
+    fixed = 0
+
     #Step 2.1 Total rows
-    while ws1[row] != '':
-        row++    
-    ws2['B2'] = row
+    while ws2['A' + rows] != '':
+        rows++    
     
-    #Step 2.2 Confirmed Reports
-    for i in row:
-#Step 2.3 Unconfirmed Reports
-#Step 2.4 Fixed Reports
-#Step 2.5 Check chart has been correctly updated
+    ws1['B2'] = row
+    
+    for row in rows:
+        #Step 2.2 Confirmed Reports
+        if ws2['P' + row] == 'confirmed':
+            confirmed += 1
+        #Step 2.3 Unconfirmed Reports
+        elif ws2['P' + row] == 'unconfirmed':
+            unconfirmed += 1
+        #Step 2.4 Fixed Reports
+        elif ws2['P' + row] == 'fixed - user':
+            fixed += 1
+        else: exit('unknown type of state')
+
+    ws1['B3'] = confirmed
+    ws1['B4'] = unconfirmed
+    ws1['B5'] = fixed
+    
+    #Step 2.5 Will need to manually check chart has been correctly updated
+    return ws1
 
 #Step 3 Update Report Month
+def Update_Month(date, ws1):
+ 
 #Step 4 Update Number of Reports per month
+def Update_Report_Range(ws1):
 #Step 4.1 Check chart has been correctly updated
 #Step 5 Get High Reported Areas
 
